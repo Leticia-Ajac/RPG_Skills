d3f825680d8423179a419c98e7e960661954c5a9
ajac <leticiajac2018@gmail.com>
2023-05-01 14:48:43 -0300

    react-router-dom

--- rpg-skills/src/App.js
+++ rpg-skills/src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './pages/home';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        hello world
+        <Router>
+          <Routes>
+            <Route path="" element={<Home/>} />
+          </Routes>
+        </Router>
       </header>
     </div>
   );
